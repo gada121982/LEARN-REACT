1f34fa9c9e1b30b71a1b597412bcf047eec0853d
Vinh Hai <gada121982@gmail.com>
2019-09-25 10:05:57 +0700

    improve feature check complete

--- src/conponents/my-app.js
+++ src/conponents/my-app.js
@@ -1,17 +1,26 @@
 import React , {Component} from 'react'
 import '../conponents/Todoitem.css';
 import classNames  from 'classnames'
+import imgSucess from  '../img/success.svg' ; 
+import imgSucessOff from  '../img/success-off.svg' ; 
 class TodoItem extends Component{
 
-    
+
     render(){
-      
-        const {item , onclick} = this.props ; 
+        
+        
+        const {item , onclick} = this.props ;
+        var imgList = imgSucessOff ; 
+        if(item.complete === 1){
+            imgList = imgSucess ; 
+        } 
         console.log("this is function onlick " , onclick); 
         return(
-            <div onClick = {onclick} className={classNames('TodoItem', {
+            <div  className={classNames('TodoItem', {
                 'TodoItem-complete' : item.complete === 1 
             })}>
+
+                <img onClick = {onclick} src={imgList} /> 
                 <p>
                     {item.title}
                 </p>
